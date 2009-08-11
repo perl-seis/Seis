945404c5c2eeaa5947be9e585353697c07f73c7b
_why <why@whytheluckystiff.net>
2009-08-11 06:04:38 -0700

    * greg.g: update bootstrapped parser to use the new grammar compiler.

--- greg.h
+++ greg.h
@@ -19,7 +19,7 @@
 #include <stdio.h>
 
 #define GREG_MAJOR	0
-#define GREG_MINOR	2
+#define GREG_MINOR	3
 #define GREG_LEVEL	0
 
 enum { Unknown= 0, Rule, Variable, Name, Dot, Character, String, Class, Action, Predicate, Alternate, Sequence, PeekFor, PeekNot, Query, Star, Plus };
