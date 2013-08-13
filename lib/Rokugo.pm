8b1d089f5c2873d451ed0d60027d490f16b9ebc6
tokuhirom <tokuhirom@gmail.com>
2013-08-13 20:22:36 +0900

    You can use rokugo library from perl5 code!

--- lib/Rokugo.pm
+++ lib/Rokugo.pm
@@ -9,6 +9,30 @@ our $VERSION = "0.01";
 use Rokugo::Exceptions;
 use Rokugo::Compiler;
 use Rokugo::Array;
+use File::Spec;
+
+my $compiler = Rokugo::Compiler->new();
+
+unshift @INC, sub {
+    my ($self, $fname) = @_;
+    (my $rg_fname = $fname) =~ s!\.pm\z!\.rg!;
+    for my $inc (@INC) {
+        next if ref $inc;
+        my $real = File::Spec->catfile($inc, $rg_fname);
+        next unless -f $real;
+
+        open my $fh, '<', $real or die $!;
+        my $code = do { local $/; <$fh> };
+        my $compiled = $compiler->compile($code) . ";1;";
+        open my $tmpfh, '<', \$compiled;
+        return ($tmpfh, sub {
+            $_ =~ s/hello/hi/;
+            $_ ? 1 : 0;
+        });
+    }
+    return;
+};
+
 
 1;
 __END__
